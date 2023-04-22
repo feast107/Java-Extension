0740054b85f604307e53bf209e80a2e82692a457
Feast <feast107@gmail.com>
2023-04-22 22:39:27 +0800

    Create StringExtension.java

---
+++ java/src/main/java/extensions/java/lang/String/StringExtension.java
@@ -0,0 +1,27 @@
+package extensions.java.lang.String;
+
+import manifold.ext.rt.api.Extension;
+import manifold.ext.rt.api.This;
+import manifold.rt.api.util.RegExpMatch;
+
+@Extension
+public class StringExtension {
+
+    /**
+     * 该 {@link String} 为 {@code null} 或者 为 {@code ""}
+     * @param string 字符串
+     * @return {@link Boolean}
+     */
+    public static boolean isNullOrEmpty(@This String string){
+        return string == null || string.isEmpty();
+    }
+
+    /**
+     * 确保非 {@code null}
+     * @param string 可能为空的字符串
+     * @return {@link String}
+     */
+    public static String ensure(@This String string){
+        return string != null ? string : "";
+    }
+}
