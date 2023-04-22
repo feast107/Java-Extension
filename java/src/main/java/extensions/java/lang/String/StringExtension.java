3dda1ab97ecb1ff7d92f0778858d8835b1c9b06e
Feast <feast107@gmail.com>
2023-04-23 01:15:05 +0800

    Update StringExtension.java

--- java/src/main/java/extensions/java/lang/String/StringExtension.java
+++ java/src/main/java/extensions/java/lang/String/StringExtension.java
@@ -1,17 +1,6 @@
 package extensions.java.lang.String;
 
-import manifold.ext.rt.ExtensionMethod;
-import manifold.ext.rt.api.Extension;
-import manifold.ext.rt.api.This;
-import org.jetbrains.annotations.Nullable;
-
-@Extension
 public class StringExtension {
-    private static final String Empty = "";
-    @Extension
-    public static String empty(){
-        return Empty;
-    }
 
     /**
      * 该 {@link String} 为 {@code null} 或者 为 {@code ""}
@@ -19,7 +8,7 @@ public class StringExtension {
      * @param string 字符串
      * @return {@link Boolean}
      */
-    public static boolean isNullOrEmpty(@This String string) {
+    public static boolean isNullOrEmpty(String string) {
         return string == null || string.isEmpty();
     }
 
@@ -29,61 +18,67 @@ public class StringExtension {
      * @param string 可能为空的字符串
      * @return {@link String}
      */
-    public static String ensure(@This String string) {
+    public static String ensure(String string) {
         return string != null ? string : "";
     }
 
     /**
      * 转换为 {@link Boolean}
+     *
      * @param string 布尔值
      * @return {@link Boolean}
      */
-    public static boolean toBoolean(@This String string){
+    public static boolean toBoolean(String string) {
         return Boolean.parseBoolean(string);
     }
 
     /**
      * 转换为 {@link Short}
+     *
      * @param string 数字
      * @return {@link Short}
      */
-    public static short toShort(@This String string){
+    public static short toShort(String string) {
         return Short.parseShort(string);
     }
 
     /**
      * 转换为 {@link Integer}
+     *
      * @param string 数字
      * @return {@link Integer}
      */
-    public static int toInt(@This String string){
+    public static int toInt(String string) {
         return Integer.parseInt(string);
     }
 
     /**
      * 转换为 {@link Long}
+     *
      * @param string 数字
      * @return {@link Long}
      */
-    public static long toLong(@This String string){
+    public static long toLong(String string) {
         return Long.parseLong(string);
     }
 
     /**
      * 转换为 {@link Float}
+     *
      * @param string 字符串
      * @return {@link Float}
      */
-    public static float toFloat(@This String string){
+    public static float toFloat(String string) {
         return Float.parseFloat(string);
     }
 
     /**
      * 转换为 {@link Double}
+     *
      * @param string 数字
      * @return {@link Double}
      */
-    public static double toDouble(@This String string){
+    public static double toDouble(String string) {
         return Double.parseDouble(string);
     }
 }
