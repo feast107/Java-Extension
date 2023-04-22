26be734f29db8d9af92b765356674b4249480541
Feast <feast107@gmail.com>
2023-04-22 22:46:26 +0800

    Update StringExtension.java

--- java/src/main/java/extensions/java/lang/String/StringExtension.java
+++ java/src/main/java/extensions/java/lang/String/StringExtension.java
@@ -8,19 +8,21 @@ public class StringExtension {
 
     /**
      * 该 {@link String} 为 {@code null} 或者 为 {@code ""}
+     *
      * @param string 字符串
      * @return {@link Boolean}
      */
-    public static boolean isNullOrEmpty(@This String string){
+    public static boolean isNullOrEmpty(@This String string) {
         return string == null || string.isEmpty();
     }
 
     /**
      * 确保非 {@code null}
+     *
      * @param string 可能为空的字符串
      * @return {@link String}
      */
-    public static String ensure(@This String string){
+    public static String ensure(@This String string) {
         return string != null ? string : "";
     }
 }
