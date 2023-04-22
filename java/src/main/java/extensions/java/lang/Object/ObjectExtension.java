4ebc1700310f94f828d3c3cc22c2d74479e2b1b6
Feast <feast107@gmail.com>
2023-04-22 22:46:24 +0800

    Update ObjectExtension.java

--- java/src/main/java/extensions/java/lang/Object/ObjectExtension.java
+++ java/src/main/java/extensions/java/lang/Object/ObjectExtension.java
@@ -9,31 +9,33 @@ import java.util.Objects;
 @Extension
 public class ObjectExtension {
     /**
-     * @exception NullPointerException 如果为空
      * @param o 对象
      * @return 对象
+     * @throws NullPointerException 如果为空
      */
-    public static @Self Object notNull(@This Object o){
+    public static @Self Object notNull(@This Object o) {
         return Objects.requireNonNull(o);
     }
 
     /**
      * 返回对象本身或者 {@link T}
-     * @param o 对象
-     * @param or 如果为空的值
-     * @return 对象
+     *
+     * @param o   对象
+     * @param or  如果为空的值
      * @param <T> 类型
+     * @return 对象
      */
-    public static <T> @Self Object ifNull(@This Object o,T or){
+    public static <T> @Self Object ifNull(@This Object o, T or) {
         return o != null ? o : or;
     }
 
     /**
      * 是否为空
+     *
      * @param o 对象
      * @return 是否为空
      */
-    public static boolean isNull(@This Object o){
+    public static boolean isNull(@This Object o) {
         return o == null;
     }
 }
