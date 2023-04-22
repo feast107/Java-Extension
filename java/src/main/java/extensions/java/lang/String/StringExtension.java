a343f54424b61d2744a3b48ae21b53adb5622cf3
Feast <feast107@gmail.com>
2023-04-23 00:23:29 +0800

    Update StringExtension.java

--- java/src/main/java/extensions/java/lang/String/StringExtension.java
+++ java/src/main/java/extensions/java/lang/String/StringExtension.java
@@ -26,4 +26,31 @@ public class StringExtension {
     public static String ensure(@This String string) {
         return string != null ? string : "";
     }
+
+    /**
+     * 转换为 {@link Integer}
+     * @param string 数字
+     * @return {@link Integer}
+     */
+    public static int toInt(@This String string){
+        return Integer.parseInt(string);
+    }
+
+    /**
+     * 转换为 {@link Long}
+     * @param string 数字
+     * @return {@link Long}
+     */
+    public static long toLong(@This String string){
+        return Long.parseLong(string);
+    }
+
+    /**
+     * 转换为 {@link Float}
+     * @param string 字符串
+     * @return {@link Float}
+     */
+    public static float toFloat(@This String string){
+        return Float.parseFloat(string);
+    }
 }
