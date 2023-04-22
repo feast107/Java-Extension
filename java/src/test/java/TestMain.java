26823073521f0a929ea74ee35aafb38c1578f2f6
Feast <feast107@gmail.com>
2023-04-22 23:27:20 +0800

    Update TestMain.java

--- java/src/test/java/TestMain.java
+++ java/src/test/java/TestMain.java
@@ -1,23 +1,27 @@
+import org.jetbrains.annotations.TestOnly;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-class Foo{
+class Foo {
     static int Count = 0;
     public int sequence = ++Count;
     public int priority = Integer.MAX_VALUE - sequence;
 
-    public String name = "This is number:" + Count + " of Foo , whose priority is:"+priority;
+    public String name = "This is number:" + Count + " of Foo , whose priority is:" + priority;
 
     @Override
-    public String toString(){
+    public String toString() {
         return name;
     }
 }
 
 public class TestMain {
-    private static final List<Foo> List = new ArrayList<>(){ };
+    private static final List<Foo> List = new ArrayList<>();
 
-    public static void main(String[] __){
+
+    public static void main(String[] __) {
         List.add(new Foo());
         List.add(new Foo());
         List.add(new Foo());
@@ -27,13 +31,32 @@ public class TestMain {
         List<Foo> temp = new ArrayList<>();
 
         List
-                .orderBy(x->x.priority)
-                .aggregate(temp,(s,i)->{ System.out.println(i); return List; })
-                .orderByDescending(x->x.sequence)
-                .aggregate(temp,(s,i)->{ System.out.println(i); return List; })
-                .where(x->x.sequence > 3)
-                .aggregate(temp,(s,i)->{ System.out.println(i); return List; })
-                .select(x->x.name)
+                .orderBy(x -> x.priority)
+                .aggregate(temp, (s, i) -> {
+                    System.out.println(i);
+                    return List;
+                })
+                .orderByDescending(x -> x.sequence)
+                .aggregate(temp, (s, i) -> {
+                    System.out.println(i);
+                    return List;
+                })
+                .where(x -> x.sequence > 3)
+                .aggregate(temp, (s, i) -> {
+                    System.out.println(i);
+                    return List;
+                })
+                .findAll(x -> x.sequence > 0)
+                .select(x -> x.name)
+                .reverse()
                 .forEach(System.out::println);
+
+
+        String str = "";
+        System.out.println(str.isNullOrEmpty());
+        str = null;
+        System.out.println(str.isNullOrEmpty());
+
+        System.out.println(List.findLastIndex(x -> x.sequence == -1));
     }
 }
